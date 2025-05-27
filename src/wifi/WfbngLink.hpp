a83313647f197ea0a030072a6f9f32057e6eeb89
floppyhammer <tannhauser_chen@outlook.com>
2025-05-27 09:51:41 +0800

    (WIP) Add adaptive link

---
+++ src/wifi/WfbngLink.hpp
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "FecChangeController.h"
+#include "SignalQualityCalculator.h"
+#include "TxFrame.h"
+
+extern "C" {
+#include "fec.h"
+}
+
+#include <map>
+#include <mutex>
+
+#include "WiFiDriver.h"
+#include "wfb-ng/rx.hpp"
+
+const u8 wfb_tx_port = 160;
+const u8 wfb_rx_port = 32;
+
+class WfbngLink {
+public:
+    WfbngLink();
+
+    int run(int wifiChannel, int bw, int fd);
+
+    void initAgg();
+
+    void stop(int fd);
+
+    std::mutex agg_mutex;
+    std::unique_ptr<AggregatorUDPv4> video_aggregator;
+    std::unique_ptr<AggregatorUDPv4> mavlink_aggregator;
+    std::unique_ptr<AggregatorUDPv4> udp_aggregator;
+
+    void start_link_quality_thread(int fd);
+
+    // Adaptive link
+    // TODO: move this to private section
+    int current_fd;
+    bool adaptive_link_enabled;
+    bool adaptive_link_should_stop{false};
+    int adaptive_tx_power;
+    std::map<int, std::unique_ptr<Rtl8812aDevice>> rtl_devices;
+    std::unique_ptr<std::thread> link_quality_thread{nullptr};
+    bool should_clear_stats{false};
+    FecChangeController fec;
+
+    void init_thread(std::unique_ptr<std::thread> &thread,
+                     const std::function<std::unique_ptr<std::thread>()> &init_func) {
+        std::unique_lock lock(thread_mutex);
+        destroy_thread(thread);
+        thread = init_func();
+    }
+
+    void destroy_thread(std::unique_ptr<std::thread> &thread) {
+        std::unique_lock lock(thread_mutex);
+        if (thread && thread->joinable()) {
+            thread->join();
+            thread = nullptr;
+        }
+    }
+
+    void stop_adaptive_link() {
+        std::unique_lock lock(thread_mutex);
+
+        if (!link_quality_thread) return;
+        this->adaptive_link_should_stop = true;
+        destroy_thread(link_quality_thread);
+    }
+
+private:
+    void stopDevice() {
+        if (rtl_devices.find(current_fd) == rtl_devices.end()) return;
+        auto dev = rtl_devices.at(current_fd).get();
+        if (dev) {
+            dev->should_stop = true;
+        }
+    }
+
+    const char *keyPath = "/data/user/0/com.openipc.pixelpilot/files/gs.key";
+    std::recursive_mutex thread_mutex;
+    std::unique_ptr<WiFiDriver> wifi_driver;
+    std::shared_ptr<TxFrame> txFrame;
+    uint32_t video_channel_id_be;
+    uint32_t mavlink_channel_id_be;
+    uint32_t udp_channel_id_be;
+
+    Logger_t log;
+    std::unique_ptr<std::thread> usb_event_thread{nullptr};
+    std::unique_ptr<std::thread> usb_tx_thread{nullptr};
+    uint32_t link_id{7669206};
+    SignalQualityCalculator rssi_calculator;
+};
