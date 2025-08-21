6716c564498819da7dd97505d7a7465b72203380
floppyhammer <tannhauser_chen@outlook.com>
2025-08-21 10:55:36 +0800

    Fix missing gstreamer plugins in AppImage

--- build-appimage.sh
+++ build-appimage.sh
@@ -7,14 +7,24 @@ make -j$(nproc)
 
 make install DESTDIR=AppDir
 
-rm linuxdeploy-x86_64.AppImage
-wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+FILE="linuxdeploy-x86_64.AppImage"
+
+if [ -f "$FILE" ]; then
+#    rm $FILE
+    echo "The file '$FILE' exists."
+else
+    echo "The file '$FILE' does not exist. Downloading..."
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+fi
 
 # Make it executable
 chmod +x linuxdeploy*.AppImage
 
 # The AppDir/share directory will not be created, we need to run this once to create it
 ./linuxdeploy-x86_64.AppImage --appdir AppDir
+
+# Hook GStreamer plugins
+bash ./linuxdeploy-plugin-gstreamer.sh
 
 # Copy icon, binary & assets
 cp ./bin/assets/logo.png ./AppDir/usr/share/icons/hicolor/128x128/apps/aviateur.png
